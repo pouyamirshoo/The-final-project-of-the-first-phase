4c3ce856ff16c7ac5a8b7ba3ccfc8ba0547e4d26
pc <poyatajhiz59@gmail.com>
2024-05-25 20:46:11 -0800

    add needed fields and annotations to Person class

--- src/main/java/entity/Person.java
+++ src/main/java/entity/Person.java
@@ -1,4 +1,52 @@
 package entity;
 
-public class Person {
+import base.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.SoftDelete;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@ToString
+@SoftDelete
+@Builder
+@MappedSuperclass
+public class Person extends BaseEntity<Integer> {
+    @Pattern(regexp = "[a-zA-Z]+")
+    @NotNull(message = "firstname can not be null")
+    String firstname;
+    @Pattern(regexp = "[a-zA-Z]+")
+    @NotNull(message = "lastname can not be null")
+    String lastname;
+    @Column(unique = true)
+    @NotNull(message = "username can not be null")
+    String username;
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#!%&*])[A-Za-z0-9@#!%&*]{8}$"
+            , message = "password has to be 8 size and must contain at least 1 lower and upper case and 1 digit and 1 char ")
+    @NotNull(message = "password can not be null")
+    String password;
+    @Pattern(regexp = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$")
+    @NotNull(message = "email can not be null")
+    String email;
+    @NotNull(message = "phoneNumber can not be null")
+    @Pattern(regexp = "^(\\+\\d{1,3}( )?)?((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$")
+    @Column(unique = true)
+    String phoneNumber;
+    @Pattern(regexp = "[a-zA-Z]+")
+    @NotNull(message = "city can not be null")
+    String city;
+    @Column(columnDefinition = "TEXT")
+    @NotNull(message = "address can not be null")
+    String address;
+    @Column(unique = true)
+    @Pattern(regexp = "^(?!(\\d)\1{3})[13-9]{4}[1346-9][013-9]{5}$")
+    @NotNull(message = "postal code can not be null")
+    String postalCode;
 }
