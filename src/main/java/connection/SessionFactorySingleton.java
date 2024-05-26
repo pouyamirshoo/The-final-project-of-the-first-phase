8266c02f805ffc0a9dfed83c97515840d3331c97
pc <poyatajhiz59@gmail.com>
2024-05-25 17:14:10 -0800

    create SessionFactorySingleton class for connection

---
+++ src/main/java/connection/SessionFactorySingleton.java
@@ -0,0 +1,25 @@
+package connection;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class SessionFactorySingleton {
+    private static class LazyHolder {
+        static SessionFactory INSTANCE;
+
+        static {
+            var registry = new StandardServiceRegistryBuilder()
+                    .configure()
+                    .build();
+
+            INSTANCE = new MetadataSources(registry)
+                    .buildMetadata()
+                    .buildSessionFactory();
+        }
+    }
+
+    public static SessionFactory getInstance() {
+        return LazyHolder.INSTANCE;
+    }
+}
