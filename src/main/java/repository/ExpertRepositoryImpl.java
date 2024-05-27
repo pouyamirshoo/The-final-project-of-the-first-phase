9bb4b607e77b5b2c023922bc93e6071d9581244c
pc <poyatajhiz59@gmail.com>
2024-05-26 17:55:25 -0800

    creat ExpertRepositoryImpl class with implements

---
+++ src/main/java/repository/ExpertRepositoryImpl.java
@@ -0,0 +1,17 @@
+package repository;
+
+import base.repository.BaseRepositoryImpl;
+import entity.Expert;
+import org.hibernate.SessionFactory;
+
+public class ExpertRepositoryImpl extends BaseRepositoryImpl<Expert,Integer>
+        implements ExpertRepository {
+    public ExpertRepositoryImpl(SessionFactory sessionFactory) {
+        super(sessionFactory);
+    }
+
+    @Override
+    public Class<Expert> getEntityClass() {
+        return null;
+    }
+}
