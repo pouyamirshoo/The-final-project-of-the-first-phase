09b53d693279a47e7244e1126b04499a78b50439
pc <poyatajhiz59@gmail.com>
2024-05-27 15:21:55 -0800

    add needed fields to SubDuty entity class

--- src/main/java/entity/SubDuty.java
+++ src/main/java/entity/SubDuty.java
@@ -1,10 +1,15 @@
 package entity;
 
 import base.entity.BaseEntity;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.SoftDelete;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -13,7 +18,20 @@ import org.hibernate.annotations.SoftDelete;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @ToString
 @SoftDelete
-@Builder
+@SuperBuilder
 @Entity
 public class SubDuty extends BaseEntity<Integer> {
+    @Column(unique = true,name = "sub_duty_name")
+    @Pattern(regexp = "[a-zA-Z]+")
+    @NotNull(message = "sub duty name can not be null")
+    String subDutyName;
+    @NotNull(message = "sub duty price can not be null")
+    Integer price;
+    @Column(columnDefinition = "TEXT")
+    @NotNull(message = "description most not null")
+    String description;
+    @ManyToOne
+    Duty duty;
+    @OneToMany(mappedBy = "subDuty",cascade = CascadeType.ALL,orphanRemoval = true,fetch = FetchType.LAZY)
+    List<Order> orders;
 }
