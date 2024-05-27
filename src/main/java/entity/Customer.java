150dfd3cf7eca783e6702d5e0da9fc34a15f22a4
pc <poyatajhiz59@gmail.com>
2024-05-27 15:28:47 -0800

    add needed fields and annotations in Customer entity class

--- src/main/java/entity/Customer.java
+++ src/main/java/entity/Customer.java
@@ -1,4 +1,33 @@
 package entity;
 
-public class Customer {
+import jakarta.persistence.*;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.SuperBuilder;
+
+import org.hibernate.annotations.SoftDelete;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@ToString
+@SoftDelete
+@SuperBuilder
+@Entity
+public class Customer extends Person {
+    @Column(name = "customer_balance")
+    Integer customerBalance;
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    List<Order> orders;
+
+    @PrePersist
+    public void defaultValues() {
+        if (customerBalance == null)
+            customerBalance = 0;
+    }
 }
