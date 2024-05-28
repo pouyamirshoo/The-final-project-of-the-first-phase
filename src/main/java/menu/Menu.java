c7009033a07c8f86e4a1cf58d4abccee175b4a1f
pc <poyatajhiz59@gmail.com>
2024-05-27 16:53:55 -0800

    add Scanner and colors to Menu class

---
+++ src/main/java/menu/Menu.java
@@ -0,0 +1,12 @@
+package menu;
+
+import java.util.Scanner;
+
+public class Menu {
+
+    private final Scanner sc = new Scanner(System.in);
+    private final String RESET = "\u001B[0m";
+    private final String YELLOW = "\u001B[33m";
+    private final String RED = "\u001B[31m";
+    private final String GREEN = "\u001B[32m";
+}
