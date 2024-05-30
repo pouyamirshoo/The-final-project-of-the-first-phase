465e79dc3fc65bc41dfbda8d59554faee718eb97
pc <poyatajhiz59@gmail.com>
2024-05-30 15:31:41 -0800

    creat findByCustomerAndCondition method in OrderServiceImpl class

--- src/main/java/service/OrderServiceImpl.java
+++ src/main/java/service/OrderServiceImpl.java
@@ -1,13 +1,29 @@
 package service;
 
+import base.exception.NotFoundException;
+import base.exception.ReturnMethodException;
 import base.service.BaseServiceImpl;
 import entity.Order;
+import entity.enums.OrderCondition;
 import org.hibernate.SessionFactory;
 import repository.OrderRepository;
+
+import java.util.List;
 
 public class OrderServiceImpl extends BaseServiceImpl<Order,Integer, OrderRepository>
         implements OrderService {
     public OrderServiceImpl(OrderRepository repository, SessionFactory sessionFactory) {
         super(repository, sessionFactory);
+    }
+
+    @Override
+    public List<Order> findByCustomerAndCondition(int id, OrderCondition orderCondition){
+        List<Order> orders;
+        try {
+            orders = repository.findByCustomerAndCondition(id,orderCondition);
+        }catch (NotFoundException e){
+            throw new ReturnMethodException("");
+        }
+        return orders;
     }
 }
