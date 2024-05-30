7b8d6e5bd17f98ffdb38fec4d76302f8a7384658
pc <poyatajhiz59@gmail.com>
2024-05-30 15:29:33 -0800

    creat findByCustomerAndCondition method in OrderRepository class

--- src/main/java/repository/OrderRepository.java
+++ src/main/java/repository/OrderRepository.java
@@ -1,7 +1,12 @@
 package repository;
 
+import base.exception.NotFoundException;
 import base.repository.BaseRepository;
 import entity.Order;
+import entity.enums.OrderCondition;
+
+import java.util.List;
 
 public interface OrderRepository extends BaseRepository<Order,Integer> {
+    List<Order> findByCustomerAndCondition(int id, OrderCondition orderCondition) throws NotFoundException;
 }
