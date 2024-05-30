71c8409054db20478f25f3936ae1e32a3e69ba9e
pc <poyatajhiz59@gmail.com>
2024-05-30 15:31:16 -0800

    creat findByCustomerAndCondition method in OrderService class

--- src/main/java/service/OrderService.java
+++ src/main/java/service/OrderService.java
@@ -1,7 +1,12 @@
 package service;
 
+import base.exception.ReturnMethodException;
 import base.service.BaseService;
 import entity.Order;
+import entity.enums.OrderCondition;
+
+import java.util.List;
 
 public interface OrderService extends BaseService<Order,Integer> {
+    List<Order> findByCustomerAndCondition(int id, OrderCondition orderCondition) throws ReturnMethodException;
 }
