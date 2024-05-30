d6987b1f3d999fbf9f3d36af4b280b5836101997
pc <poyatajhiz59@gmail.com>
2024-05-30 15:30:03 -0800

    creat findByCustomerAndCondition method in OrderRepositoryImpl class

--- src/main/java/repository/OrderRepositoryImpl.java
+++ src/main/java/repository/OrderRepositoryImpl.java
@@ -1,8 +1,15 @@
 package repository;
 
+import base.exception.NotFoundException;
 import base.repository.BaseRepositoryImpl;
+import connection.SessionFactorySingleton;
 import entity.Order;
+import entity.enums.OrderCondition;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+import java.util.List;
 
 public class OrderRepositoryImpl extends BaseRepositoryImpl<Order,Integer>
         implements OrderRepository {
@@ -13,5 +20,17 @@ public class OrderRepositoryImpl extends BaseRepositoryImpl<Order,Integer>
     @Override
     public Class<Order> getEntityClass() {
         return null;
+    }
+
+    @Override
+    public List<Order> findByCustomerAndCondition(int id, OrderCondition orderCondition) throws NotFoundException {
+        Session session = SessionFactorySingleton.getInstance().openSession();
+        Query<Order> query = session.createQuery(" FROM Order o WHERE o.customer.id =:id AND o.orderCondition =:orderCondition",
+                Order.class);
+        query.setParameter("id",id);
+        query.setParameter("orderCondition",orderCondition);
+        List<Order> orders = query.list();
+        session.close();
+        return orders;
     }
 }
