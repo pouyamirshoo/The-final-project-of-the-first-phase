0bbb0c31842dc8984517e6ac830b431a3eea589e
pc <poyatajhiz59@gmail.com>
2024-05-31 10:16:22 -0800

    add findByUsernameAndPassword method in CustomerService class

--- src/main/java/service/CustomerService.java
+++ src/main/java/service/CustomerService.java
@@ -1,7 +1,9 @@
 package service;
 
+import base.exception.ReturnMethodException;
 import base.service.BaseService;
 import entity.Customer;
 
 public interface CustomerService extends BaseService<Customer,Integer> {
+    Customer findByUsernameAndPassword(String username,String password) throws ReturnMethodException;
 }
