cacbafe6c9ae86dd8fa0f25b0a6892885384d5e0
pc <poyatajhiz59@gmail.com>
2024-05-31 10:51:40 -0800

    add subDuties method to SubDutyService class

--- src/main/java/service/SubDutyService.java
+++ src/main/java/service/SubDutyService.java
@@ -3,5 +3,8 @@ package service;
 import base.service.BaseService;
 import entity.SubDuty;
 
+import java.util.List;
+
 public interface SubDutyService extends BaseService<SubDuty,Integer> {
+    List<SubDuty> subDuties(int id);
 }
