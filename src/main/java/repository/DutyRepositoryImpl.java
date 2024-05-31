516a40521a710d13332c13ac0799ed41cded7a55
pc <poyatajhiz59@gmail.com>
2024-05-31 10:17:30 -0800

    add duties method in DutyRepositoryImpl class

--- src/main/java/repository/DutyRepositoryImpl.java
+++ src/main/java/repository/DutyRepositoryImpl.java
@@ -1,8 +1,13 @@
 package repository;
 
 import base.repository.BaseRepositoryImpl;
+import connection.SessionFactorySingleton;
 import entity.Duty;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+import java.util.List;
 
 public class DutyRepositoryImpl extends BaseRepositoryImpl<Duty,Integer>
         implements DutyRepository {
@@ -13,5 +18,15 @@ public class DutyRepositoryImpl extends BaseRepositoryImpl<Duty,Integer>
     @Override
     public Class<Duty> getEntityClass() {
         return null;
+    }
+
+    @Override
+    public List<Duty> duties(){
+        Session session = SessionFactorySingleton.getInstance().openSession();
+        Query<Duty> query = session.createQuery(" FROM Duty d ",
+                Duty.class);
+        List<Duty> duties = query.list();
+        session.close();
+        return duties;
     }
 }
