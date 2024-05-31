0626732ee6f1f133a9c0209577557df38dbc0799
pc <poyatajhiz59@gmail.com>
2024-05-31 10:51:27 -0800

    add subDuties method to SubDutyRepositoryImpl class

--- src/main/java/repository/SubDutyRepositoryImpl.java
+++ src/main/java/repository/SubDutyRepositoryImpl.java
@@ -1,11 +1,17 @@
 package repository;
 
 import base.repository.BaseRepositoryImpl;
+import connection.SessionFactorySingleton;
 import entity.SubDuty;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+import java.util.List;
 
 
-public class SubDutyRepositoryImpl extends BaseRepositoryImpl<SubDuty,Integer> implements SubDutyRepository {
+public class SubDutyRepositoryImpl extends BaseRepositoryImpl<SubDuty,Integer>
+        implements SubDutyRepository {
     public SubDutyRepositoryImpl(SessionFactory sessionFactory) {
         super(sessionFactory);
     }
@@ -13,5 +19,16 @@ public class SubDutyRepositoryImpl extends BaseRepositoryImpl<SubDuty,Integer> i
     @Override
     public Class<SubDuty> getEntityClass() {
         return null;
+    }
+
+    @Override
+    public List<SubDuty> subDuties(int id) {
+        Session session = SessionFactorySingleton.getInstance().openSession();
+        Query<SubDuty> query = session.createQuery(" FROM SubDuty s WHERE s.duty.id =:id",
+                SubDuty.class);
+        query.setParameter("id",id);
+        List<SubDuty> subDuties = query.list();
+        session.close();
+        return subDuties;
     }
 }
