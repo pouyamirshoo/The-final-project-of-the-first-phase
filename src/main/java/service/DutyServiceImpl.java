6785f70366c1592a4f8b41f3aae73a2d13f52401
pc <poyatajhiz59@gmail.com>
2024-05-31 10:18:05 -0800

    add duties method in DutyServiceImpl class

--- src/main/java/service/DutyServiceImpl.java
+++ src/main/java/service/DutyServiceImpl.java
@@ -5,9 +5,16 @@ import entity.Duty;
 import org.hibernate.SessionFactory;
 import repository.DutyRepository;
 
+import java.util.List;
+
 public class DutyServiceImpl extends BaseServiceImpl<Duty,Integer, DutyRepository>
         implements DutyService {
     public DutyServiceImpl(DutyRepository repository, SessionFactory sessionFactory) {
         super(repository, sessionFactory);
+    }
+
+    @Override
+    public List<Duty> duties() {
+        return repository.duties();
     }
 }
