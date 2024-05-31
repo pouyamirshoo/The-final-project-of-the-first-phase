d87f86858ad509b4961f8f03ff9be435eef6c5af
pc <poyatajhiz59@gmail.com>
2024-05-31 10:17:07 -0800

    add duties method in DutyRepository class

--- src/main/java/repository/DutyRepository.java
+++ src/main/java/repository/DutyRepository.java
@@ -3,5 +3,8 @@ package repository;
 import base.repository.BaseRepository;
 import entity.Duty;
 
+import java.util.List;
+
 public interface DutyRepository extends BaseRepository<Duty,Integer> {
+     List<Duty> duties();
 }
