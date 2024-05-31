584a56b88882199bc2b937d8b0be27bef556363b
pc <poyatajhiz59@gmail.com>
2024-05-31 10:51:15 -0800

    add subDuties method to SubDutyRepository class

--- src/main/java/repository/SubDutyRepository.java
+++ src/main/java/repository/SubDutyRepository.java
@@ -3,5 +3,8 @@ package repository;
 import base.repository.BaseRepository;
 import entity.SubDuty;
 
+import java.util.List;
+
 public interface SubDutyRepository extends BaseRepository<SubDuty,Integer> {
+    List<SubDuty> subDuties(int id);
 }
