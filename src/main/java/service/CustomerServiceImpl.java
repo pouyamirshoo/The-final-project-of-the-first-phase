6b5f2756b24fcdc7f632c494b067c98b7abff41b
pc <poyatajhiz59@gmail.com>
2024-05-31 10:16:36 -0800

    add findByUsernameAndPassword method in CustomerServiceImpl class

--- src/main/java/service/CustomerServiceImpl.java
+++ src/main/java/service/CustomerServiceImpl.java
@@ -1,5 +1,7 @@
 package service;
 
+import base.exception.NotFoundException;
+import base.exception.ReturnMethodException;
 import base.service.BaseServiceImpl;
 import entity.Customer;
 import org.hibernate.SessionFactory;
@@ -9,5 +11,16 @@ public class CustomerServiceImpl extends BaseServiceImpl<Customer,Integer,Custom
         implements CustomerService {
     public CustomerServiceImpl(CustomerRepository repository, SessionFactory sessionFactory) {
         super(repository, sessionFactory);
+    }
+
+    @Override
+    public Customer findByUsernameAndPassword(String username, String password) {
+        Customer customer;
+        try {
+           customer = repository.findByUsernameAndPassword(username,password);
+        } catch (NotFoundException e){
+            throw new ReturnMethodException("");
+        }
+        return customer;
     }
 }
