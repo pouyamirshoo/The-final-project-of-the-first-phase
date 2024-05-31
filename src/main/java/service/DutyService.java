091d3c89c75e23303bf5efbc4cbbb4977e4252df
pc <poyatajhiz59@gmail.com>
2024-05-31 10:17:50 -0800

    add duties method in DutyService class

--- src/main/java/service/DutyService.java
+++ src/main/java/service/DutyService.java
@@ -3,5 +3,8 @@ package service;
 import base.service.BaseService;
 import entity.Duty;
 
+import java.util.List;
+
 public interface DutyService extends BaseService<Duty,Integer> {
+    List<Duty> duties();
 }
