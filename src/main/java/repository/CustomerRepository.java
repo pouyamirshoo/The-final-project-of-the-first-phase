37b2c24499d935b11658af6f33f212fbf1ff2aa9
pc <poyatajhiz59@gmail.com>
2024-05-31 10:15:37 -0800

    add findByUsernameAndPassword method in CustomerRepository class

--- src/main/java/repository/CustomerRepository.java
+++ src/main/java/repository/CustomerRepository.java
@@ -1,7 +1,9 @@
 package repository;
 
+import base.exception.NotFoundException;
 import base.repository.BaseRepository;
 import entity.Customer;
 
 public interface CustomerRepository extends BaseRepository<Customer,Integer> {
+    Customer findByUsernameAndPassword(String username,String password) throws NotFoundException;
 }
