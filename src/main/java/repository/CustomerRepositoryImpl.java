e45cf46e038062f131ceefa1272f0c81ce2dde56
pc <poyatajhiz59@gmail.com>
2024-05-31 10:16:02 -0800

    add findByUsernameAndPassword method in CustomerRepositoryImpl class

--- src/main/java/repository/CustomerRepositoryImpl.java
+++ src/main/java/repository/CustomerRepositoryImpl.java
@@ -1,8 +1,12 @@
 package repository;
 
+import base.exception.NotFoundException;
 import base.repository.BaseRepositoryImpl;
+import connection.SessionFactorySingleton;
 import entity.Customer;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 
 public class CustomerRepositoryImpl extends BaseRepositoryImpl<Customer,Integer>
         implements CustomerRepository {
@@ -13,5 +17,18 @@ public class CustomerRepositoryImpl extends BaseRepositoryImpl<Customer,Integer>
     @Override
     public Class<Customer> getEntityClass() {
         return null;
+    }
+
+
+    @Override
+    public Customer findByUsernameAndPassword(String username, String password) throws NotFoundException {
+        Session session = SessionFactorySingleton.getInstance().openSession();
+        Query<Customer> query = session.createQuery("FROM Customer c WHERE c.username = :username " +
+                "AND c.password = :password", Customer.class);
+        query.setParameter("username", username);
+        query.setParameter("password", password);
+        Customer customer = query.uniqueResult();
+        session.close();
+        return customer;
     }
 }
