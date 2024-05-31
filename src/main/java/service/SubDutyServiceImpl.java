9b52d806a0eeb1d3ab9ddbe0e6c1eff23c1c943e
pc <poyatajhiz59@gmail.com>
2024-05-31 10:51:54 -0800

    add subDuties method to SubDutyServiceImpl class

--- src/main/java/service/SubDutyServiceImpl.java
+++ src/main/java/service/SubDutyServiceImpl.java
@@ -5,9 +5,16 @@ import entity.SubDuty;
 import org.hibernate.SessionFactory;
 import repository.SubDutyRepository;
 
+import java.util.List;
+
 public class SubDutyServiceImpl extends BaseServiceImpl<SubDuty,Integer, SubDutyRepository>
         implements SubDutyService {
     public SubDutyServiceImpl(SubDutyRepository repository, SessionFactory sessionFactory) {
         super(repository, sessionFactory);
+    }
+
+    @Override
+    public List<SubDuty> subDuties(int id) {
+        return repository.subDuties(id);
     }
 }
