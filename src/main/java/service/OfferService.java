12e1d8a6a0f8026bb887c7cad9cddc992c46c72f
pc <poyatajhiz59@gmail.com>
2024-06-01 19:55:24 -0800

    add findByOrderId method in OfferService

--- src/main/java/service/OfferService.java
+++ src/main/java/service/OfferService.java
@@ -1,7 +1,11 @@
 package service;
 
+import base.exception.ReturnMethodException;
 import base.service.BaseService;
 import entity.Offer;
 
+import java.util.List;
+
 public interface OfferService extends BaseService<Offer,Integer> {
+    List<Offer> findByOrderId(int id) throws ReturnMethodException;
 }
