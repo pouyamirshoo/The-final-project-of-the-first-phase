c4cb297734f196b9702c72e33f563942bfbcab2c
pc <poyatajhiz59@gmail.com>
2024-06-01 19:17:59 -0800

    make findByUsernameAndPassword method Optional in CustomerRepository

--- src/main/java/repository/CustomerRepository.java
+++ src/main/java/repository/CustomerRepository.java
@@ -4,6 +4,8 @@ import base.exception.NotFoundException;
 import base.repository.BaseRepository;
 import entity.Customer;
 
+import java.util.Optional;
+
 public interface CustomerRepository extends BaseRepository<Customer,Integer> {
-    Customer findByUsernameAndPassword(String username,String password) throws NotFoundException;
+    Optional<Customer> findByUsernameAndPassword(String username, String password) throws NotFoundException;
 }
