cf6937b1f5ce950c7a53cce1b59acb325b9404fc
pc <poyatajhiz59@gmail.com>
2024-06-01 19:19:45 -0800

    change exception to findByUsernameAndPassword method CustomerService

--- src/main/java/service/CustomerServiceImpl.java
+++ src/main/java/service/CustomerServiceImpl.java
@@ -1,13 +1,13 @@
 package service;
 
 import base.exception.NotFoundException;
-import base.exception.ReturnMethodException;
 import base.service.BaseServiceImpl;
 import entity.Customer;
 import org.hibernate.SessionFactory;
 import repository.CustomerRepository;
 
-public class CustomerServiceImpl extends BaseServiceImpl<Customer,Integer,CustomerRepository>
+
+public class CustomerServiceImpl extends BaseServiceImpl<Customer, Integer, CustomerRepository>
         implements CustomerService {
     public CustomerServiceImpl(CustomerRepository repository, SessionFactory sessionFactory) {
         super(repository, sessionFactory);
@@ -15,12 +15,7 @@ public class CustomerServiceImpl extends BaseServiceImpl<Customer,Integer,Custom
 
     @Override
     public Customer findByUsernameAndPassword(String username, String password) {
-        Customer customer;
-        try {
-           customer = repository.findByUsernameAndPassword(username,password);
-        } catch (NotFoundException e){
-            throw new ReturnMethodException("");
-        }
-        return customer;
+
+        return repository.findByUsernameAndPassword(username, password).orElseThrow(() -> new NotFoundException(""));
     }
 }
