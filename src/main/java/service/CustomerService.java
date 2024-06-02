ce0b4d77dd44b63c50f233459cd591cbe733e4a1
pc <poyatajhiz59@gmail.com>
2024-06-01 19:19:20 -0800

    change exception to findByUsernameAndPassword method CustomerService

--- src/main/java/service/CustomerService.java
+++ src/main/java/service/CustomerService.java
@@ -1,9 +1,8 @@
 package service;
 
-import base.exception.ReturnMethodException;
 import base.service.BaseService;
 import entity.Customer;
 
 public interface CustomerService extends BaseService<Customer,Integer> {
-    Customer findByUsernameAndPassword(String username,String password) throws ReturnMethodException;
+    Customer findByUsernameAndPassword(String username,String password);
 }
