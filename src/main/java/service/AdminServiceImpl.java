78a50d6b14abfb4b7f6796fbf8f23fe947da69c5
pc <poyatajhiz59@gmail.com>
2024-06-01 19:12:18 -0800

    add findByUsernameAndPassword method to AdminServiceImpl

--- src/main/java/service/AdminServiceImpl.java
+++ src/main/java/service/AdminServiceImpl.java
@@ -1,5 +1,6 @@
 package service;
 
+import base.exception.NotFoundException;
 import base.service.BaseServiceImpl;
 import entity.Admin;
 import org.hibernate.SessionFactory;
@@ -9,5 +10,10 @@ public class AdminServiceImpl extends BaseServiceImpl<Admin,Integer, AdminReposi
         implements AdminService {
     public AdminServiceImpl(AdminRepository repository, SessionFactory sessionFactory) {
         super(repository, sessionFactory);
+    }
+
+    @Override
+    public Admin findByUsernameAndPassword(String username, String password) {
+        return repository.findByUsernameAndPassword(username,password).orElseThrow(() -> new NotFoundException(""));
     }
 }
