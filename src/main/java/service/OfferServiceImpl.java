abee840a111d69f5d12fe0aff5c54e35a088656a
pc <poyatajhiz59@gmail.com>
2024-06-01 19:56:13 -0800

    add findByOrderId method in OfferServiceImpl

--- src/main/java/service/OfferServiceImpl.java
+++ src/main/java/service/OfferServiceImpl.java
@@ -1,13 +1,28 @@
 package service;
 
+import base.exception.NotFoundException;
+import base.exception.ReturnMethodException;
 import base.service.BaseServiceImpl;
 import entity.Offer;
 import org.hibernate.SessionFactory;
 import repository.OfferRepository;
 
+import java.util.List;
+
 public class OfferServiceImpl extends BaseServiceImpl<Offer,Integer, OfferRepository>
         implements OfferService {
     public OfferServiceImpl(OfferRepository repository, SessionFactory sessionFactory) {
         super(repository, sessionFactory);
+    }
+
+    @Override
+    public List<Offer> findByOrderId(int id) {
+        List<Offer> offers;
+        try {
+            offers = repository.findByOrderId(id);
+        } catch (NotFoundException e){
+            throw new ReturnMethodException("");
+        }
+        return offers;
     }
 }
