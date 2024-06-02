9ea7e4f793549463f93952501bfcfb6a68dcdf8d
pc <poyatajhiz59@gmail.com>
2024-06-01 19:10:50 -0800

    add findByUsernameAndPassword method to AdminRepository

--- src/main/java/repository/AdminRepository.java
+++ src/main/java/repository/AdminRepository.java
@@ -1,7 +1,11 @@
 package repository;
 
+import base.exception.NotFoundException;
 import base.repository.BaseRepository;
 import entity.Admin;
 
+import java.util.Optional;
+
 public interface AdminRepository extends BaseRepository<Admin,Integer> {
+    Optional<Admin> findByUsernameAndPassword(String username, String password) throws NotFoundException;
 }
