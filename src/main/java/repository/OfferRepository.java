71d75e3ef3515f5ecba1968ee616e3e0323c8195
pc <poyatajhiz59@gmail.com>
2024-06-01 19:54:39 -0800

    add findByOrderId method in OfferRepository

--- src/main/java/repository/OfferRepository.java
+++ src/main/java/repository/OfferRepository.java
@@ -1,7 +1,11 @@
 package repository;
 
+import base.exception.NotFoundException;
 import base.repository.BaseRepository;
 import entity.Offer;
 
+import java.util.List;
+
 public interface OfferRepository extends BaseRepository<Offer,Integer> {
+    List<Offer> findByOrderId(int id) throws NotFoundException;
 }
