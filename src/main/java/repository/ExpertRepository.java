e46a676bd4735f9ebf57a8c5e803dd04cd267b30
pc <poyatajhiz59@gmail.com>
2024-06-06 13:04:48 -0800

    add needed three find methods in ExpertRepository

--- src/main/java/repository/ExpertRepository.java
+++ src/main/java/repository/ExpertRepository.java
@@ -1,7 +1,15 @@
 package repository;
 
+import base.exception.NotFoundException;
 import base.repository.BaseRepository;
 import entity.Expert;
+import entity.enums.ExpertCondition;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ExpertRepository extends BaseRepository<Expert,Integer> {
+    Optional<Expert> findByUsernameAndPassword(String username, String password) throws NotFoundException;
+    List<Expert> findByCondition(ExpertCondition expertCondition) throws NullPointerException;
+    List<Expert> allExperts() throws NullPointerException;
 }
