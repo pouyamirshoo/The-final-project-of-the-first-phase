759bc35540681ae8b4225577efa57a4016f1f103
pc <poyatajhiz59@gmail.com>
2024-06-06 13:05:26 -0800

    add needed three find methods in ExpertService

--- src/main/java/service/ExpertService.java
+++ src/main/java/service/ExpertService.java
@@ -1,7 +1,16 @@
 package service;
 
+import base.exception.ReturnMethodException;
 import base.service.BaseService;
 import entity.Expert;
+import entity.enums.ExpertCondition;
 
-public interface ExpertService extends BaseService<Expert,Integer> {
+import java.util.List;
+
+public interface ExpertService extends BaseService<Expert, Integer> {
+    Expert findByUsernameAndPassword(String username, String password);
+
+    List<Expert> findByCondition(ExpertCondition expertCondition) throws ReturnMethodException;
+
+    List<Expert> allExperts() throws ReturnMethodException;
 }
