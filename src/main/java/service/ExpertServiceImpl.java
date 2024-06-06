2724303081705a7fe28ea7571c46f9f6282a750b
pc <poyatajhiz59@gmail.com>
2024-06-06 13:05:35 -0800

    add needed three find methods in ExpertServiceImpl

--- src/main/java/service/ExpertServiceImpl.java
+++ src/main/java/service/ExpertServiceImpl.java
@@ -1,13 +1,45 @@
 package service;
 
+import base.exception.NotFoundException;
+import base.exception.ReturnMethodException;
 import base.service.BaseServiceImpl;
 import entity.Expert;
+import entity.enums.ExpertCondition;
 import org.hibernate.SessionFactory;
 import repository.ExpertRepository;
+
+import java.util.List;
 
 public class ExpertServiceImpl extends BaseServiceImpl<Expert,Integer, ExpertRepository>
         implements ExpertService {
     public ExpertServiceImpl(ExpertRepository repository, SessionFactory sessionFactory) {
         super(repository, sessionFactory);
+    }
+
+    @Override
+    public Expert findByUsernameAndPassword(String username, String password) {
+        return repository.findByUsernameAndPassword(username, password).orElseThrow(() -> new NotFoundException(""));
+    }
+
+    @Override
+    public List<Expert> findByCondition(ExpertCondition expertCondition) throws ReturnMethodException {
+        List<Expert> experts;
+        try {
+            experts = repository.findByCondition(expertCondition);
+        } catch (NullPointerException e) {
+            throw new ReturnMethodException("");
+        }
+        return experts;
+    }
+
+    @Override
+    public List<Expert> allExperts() throws ReturnMethodException {
+        List<Expert> experts;
+        try {
+            experts = repository.allExperts();
+        } catch (NullPointerException e) {
+            throw new ReturnMethodException("");
+        }
+        return experts;
     }
 }
