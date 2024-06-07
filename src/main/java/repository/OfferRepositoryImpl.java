064e013bc0e8d287523dda77eba11b0499bca48b
pc <poyatajhiz59@gmail.com>
2024-06-06 17:26:20 -0800

    remove unused method from OfferRepositoryImpl

--- src/main/java/repository/OfferRepositoryImpl.java
+++ src/main/java/repository/OfferRepositoryImpl.java
@@ -1,6 +1,5 @@
 package repository;
 
-import base.exception.NotFoundException;
 import base.repository.BaseRepositoryImpl;
 import connection.SessionFactorySingleton;
 import entity.Offer;
@@ -9,9 +8,8 @@ import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 
 import java.util.List;
-import java.util.Optional;
 
-public class OfferRepositoryImpl extends BaseRepositoryImpl<Offer,Integer>
+public class OfferRepositoryImpl extends BaseRepositoryImpl<Offer, Integer>
         implements OfferRepository {
     public OfferRepositoryImpl(SessionFactory sessionFactory) {
         super(sessionFactory);
@@ -23,20 +21,7 @@ public class OfferRepositoryImpl extends BaseRepositoryImpl<Offer,Integer>
     }
 
     @Override
-    public Optional<Offer> findByOrderId(int id) throws NotFoundException {
-        Session session = SessionFactorySingleton.getInstance().openSession();
-        Query<Offer> query = session.createQuery("FROM Offer o WHERE o.order.id = :id ", Offer.class);
-        query.setParameter("id", id);
-        Optional<Offer> optionalOffer = query.setMaxResults(1)
-                .getResultList()
-                .stream()
-                .findFirst();
-        session.close();
-        return optionalOffer;
-    }
-
-    @Override
-    public List<Offer> findAllOfOneOrderOffers(int id) throws NullPointerException{
+    public List<Offer> findAllOfOneOrderOffers(int id) throws NullPointerException {
         Session session = SessionFactorySingleton.getInstance().openSession();
         Query<Offer> query = session.createQuery("FROM Offer o WHERE o.order.id = :id ", Offer.class);
         query.setParameter("id", id);
