5f753d7053ae4726215087efea9434192e81d288
pc <poyatajhiz59@gmail.com>
2024-06-06 17:25:24 -0800

    add two find methods to OfferServiceImpl class

--- src/main/java/service/OfferServiceImpl.java
+++ src/main/java/service/OfferServiceImpl.java
@@ -1,6 +1,5 @@
 package service;
 
-import base.exception.NotFoundException;
 import base.exception.ReturnMethodException;
 import base.service.BaseServiceImpl;
 import entity.Offer;
@@ -9,18 +8,29 @@ import repository.OfferRepository;
 
 import java.util.List;
 
-public class OfferServiceImpl extends BaseServiceImpl<Offer,Integer, OfferRepository>
+public class OfferServiceImpl extends BaseServiceImpl<Offer, Integer, OfferRepository>
         implements OfferService {
     public OfferServiceImpl(OfferRepository repository, SessionFactory sessionFactory) {
         super(repository, sessionFactory);
     }
 
     @Override
-    public List<Offer> findByOrderId(int id) {
+    public List<Offer> findAllOfOneOrderOffers(int id) {
         List<Offer> offers;
         try {
-            offers = repository.findByOrderId(id);
-        } catch (NotFoundException e){
+            offers = repository.findAllOfOneOrderOffers(id);
+        } catch (NullPointerException e) {
+            throw new ReturnMethodException("");
+        }
+        return offers;
+    }
+
+    @Override
+    public List<Offer> findAllExpertOffers(int id) {
+        List<Offer> offers;
+        try {
+            offers = repository.findAllExpertOffers(id);
+        } catch (NullPointerException e) {
             throw new ReturnMethodException("");
         }
         return offers;
